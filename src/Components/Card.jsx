2e52f0bbe1ba70ebd741e40e36ccdcc075a2a38c
iliya <iliyahm@yahoo.com>
2024-03-09 16:13:01 +0000

    first slider react

---
+++ src/Components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import CardImage from './CardImage'
+import CardItem from './CardItem'
+import UnitStates from './UnitStates'
+
+export default function Card() {
+  return (
+    <div className="clash-card barbarian">
+           <CardImage />
+
+<CardItem type='clash-card__level clash-card__level--barbarian'>Level 4</CardItem>
+<CardItem type='clash-card__unit-name'>The Barbarian</CardItem>
+<CardItem type='clash-card__unit-description'>   The Barbarian is a kilt-clad Scottish warrior with an angry, battle-ready expression, hungry for
+                destruction. He has Killer yellow horseshoe mustache.</CardItem>
+
+<UnitStates />
+      
+ 
+   
+
+        </div> 
+  )
+}
